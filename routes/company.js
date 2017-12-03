5afa0f2e70c4c9a08d5503c851532fe36967a9eb
Jose Luis Salazar Gonzalez <joseluis25sg@gmail.com>
2017-12-04 00:51:29 +0100

    Add company creation route
    Add company creation route: POST /company.

--- routes/company.js
+++ routes/company.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json({ type: 'application/json'});
 
 var companyController = require('../controllers/company');
+
+router.route('/')
+  .post(jsonParser, companyController.createCompany);
 
 router.route('/:id')
   .get(companyController.getCompanyById)
